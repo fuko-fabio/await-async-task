3d42c6a58d9275b182d918601ec60f3827081396
Norbert Pabian <norbert.pabian@britishpearl.com>
2020-07-06 11:34:48 +0200

    fix: dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export function asyncTasks (timeoutMs: number): Promise<void> {
+export function asyncTasks (timeoutMs: number = 0): Promise<void> {
     return new Promise((resolve) => setTimeout(function () { process.nextTick(resolve); }, timeoutMs));
 }
 
